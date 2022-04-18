18a20e486aa7ab5830edac4f6675c2b9bd7eace4
flleonx <flleondev@gmail.com>
2022-04-17 19:59:16 -0500

    control props first commit

---
+++ intermediate/05-control-props-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,73 @@
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from "../components";
+
+import { Product } from "../interfaces/interfaces";
+
+import "../styles/custom-styles.css";
+
+const product1 = {
+  id: "1",
+  title: "Coffee Mug - Card",
+  img: "./coffee-mug.png",
+};
+
+const product2 = {
+  id: "2",
+  title: "Coffee Mug - Meme",
+  img: "./coffee-mug2.png",
+};
+
+const products: Product[] = [product1, product2];
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="bg-dark text-white"
+          >
+            <ProductImage
+              className="custom-image"
+              style={{
+                boxShadow: "10px 10px 10px rgba(0,0,0,0.2)",
+              }}
+            />
+            <ProductTitle className="text-bold" />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
+      </div>
+
+      <div className="shopping-cart">
+        <ProductCard
+          product={product2}
+          className="bg-dark text-white"
+          style={{ width: '100px' }}
+        >
+          <ProductImage
+            className="custom-image"
+            style={{
+              boxShadow: "10px 10px 10px rgba(0,0,0,0.2)",
+            }}
+          />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+      </div>
+    </div>
+  );
+};
